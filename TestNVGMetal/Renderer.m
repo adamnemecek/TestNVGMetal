3b1c821f7b33fcabe2eb45ae5d1b5f98bbd639e5
Adam Nemecek <adamnemecek@gmail.com>
2020-08-03 09:07:27 -0700

    render rectangle

--- TestNVGMetal/Renderer.m
+++ TestNVGMetal/Renderer.m
@@ -23,11 +23,13 @@
   
   mnvgClearWithColor(ctx, nvgRGBA(255,128,0,255));
   nvgBeginFrame(ctx, w, h, [[view window] backingScaleFactor]);
-  
-  nvgRect(ctx, 10, 10, w - 20, h - 20);
-  nvgStrokeWidth(ctx, 1.);
-  nvgStrokeColor(ctx, nvgRGBA(0,0,0,255));
-  nvgStroke(ctx);
+
+  nvgFillColor(ctx, nvgRGBA(28,30,34,192));
+  nvgRect(ctx, 100, 100, 40, 40);
+    nvgFill(ctx);
+  //nvgStrokeWidth(ctx, 1.);
+//  nvgStrokeColor(ctx, nvgRGBA(0,0,0,255));
+//  nvgStroke(ctx);
   
   nvgEndFrame(ctx);
 }
